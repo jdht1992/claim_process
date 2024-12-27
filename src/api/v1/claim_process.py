0e9a5c97d6c4a920c4631416f8019cec75f73cb9
Juan Herrera <cripto1989@gmail.com>
2024-12-27 16:39:01 -0600

    Refactor claim process

--- src/api/v1/claim_process.py
+++ src/api/v1/claim_process.py
@@ -1,29 +1,29 @@
 from fastapi import APIRouter, status
 from sqlmodel import select
 
-from src.models import ClaimProcess
-from src.schemas import ClaimProcessCreateSchema
+from src.models import Claim, ClaimItem
+from src.schemas import ClaimItemSchema
 from db import SessionDep
 
 router = APIRouter()
 
 
-@router.get("/", status_code=status.HTTP_200_OK, response_model=list[ClaimProcess])
-async def list_claim(session: SessionDep):
-    return session.exec(select(ClaimProcess)).all()
+# @router.get("/", status_code=status.HTTP_200_OK, response_model=list[ClaimProcess])
+# async def list_claim(session: SessionDep):
+#     return session.exec(select(ClaimProcess)).all()
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def get_user(claims_create: ClaimProcessCreateSchema, session: SessionDep):
-    cliams = claims_create.model_dump().get("cliam")
+def get_user(claim_items: list[ClaimItemSchema], session: SessionDep):
 
-    for claim in cliams:
-        print(claim)
-        claim = ClaimProcess.model_validate(claim)
-        session.add(claim)
+    claim = Claim()
+    session.add(claim)
+    session.commit()
+    print(f"Created claim with id {claim.id}")
+
+    for item in claim_items:
+        item.claim_uid = claim.id
+        item = ClaimItem.model_validate(item)
+        session.add(item)
         session.commit()
-        #session.refresh(claim)
-
-        print(claim)
-
-    return cliams
+    return {}
